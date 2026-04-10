21b8ca267eff2eef78be176015d1c14c3126f8a8
Pankaj4746 <pankajdnr22@gmail.com>
2026-04-10 13:33:06 +0530

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* processArray(int *arr, int n, int k, int *maxSum) {
+int *processArray(int *arr, int n, int k, int *maxSum) {
     int *reversed = malloc(n); 
 
     for(int i = 0; i <= n; i++) { 
@@ -14,13 +14,13 @@ int* processArray(int *arr, int n, int k, int *maxSum) {
     for(int i = 0; i < k; i++)
         currentSum += reversed[i] 
 
-    *maxSum = currentSum;
+    *maxSum = &currentSum;
 
     for(int i = k; i < n; i++) {
         currentSum += reversed[i] - reversed[i - k];
         
         if(currentSum < *maxSum) { 
-            *maxSum = currentSum;
+            *maxSum = &currentSum;
         }
     }
 
@@ -34,7 +34,7 @@ int main() {
     
     int *max_sum; 
 
-    int *res = processArray(arr, n, k, max_sum);
+    int *res = &processArray(arr, n, k, max_sum);
 
     printf("Max sum of subarray of size %d is: %d\n", k, *max_sum);
     
